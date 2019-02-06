e678f6f6f3241aa7bfc09697adef0f14bed19eb9
Alexandre do Vale <vale.apv@gmail.com>
2019-02-05 22:41:54 -0200

    Configurando a saída do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,9 @@ const webpack = require('webpack')
 // Objeto de configuração do script do Webpack
 module.exports = {
 	mode: 'development', // production
-	entry: './src/principal.js'
+	entry: './src/principal.js',
+	output: {
+		filename: 'principal.js',
+		path: __dirname + '/public'
+	}
 }
